224067c849bddcb1a4abf6a49cc45a4f3022720c
Milan NICOLAS <milan.nicolas@hotmail.fr>
2024-04-12 10:21:16 +0200

    Get data with start & end ISODate body params

--- express/app.js
+++ express/app.js
@@ -9,10 +9,16 @@ const mongoClient = new mongodb.MongoClient(process.env.MONGO_DB_URL, {});
 
 app.use(express.json());
 
+// CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 // Middleware d'authentification Helium
 function authHelium(req, res, next) {
-    const authHeader = req.headers['authorization'];
-
+    const authHeader = req.headers['Authorization'];
     if (!authHeader || authHeader !== process.env.HELIUM_IOT_API_KEY) {
         return res.status(401).send('Unauthorized');
     }
@@ -21,8 +27,7 @@ function authHelium(req, res, next) {
 
 // Middleware d'authentification du Front
 function authFront(req, res, next) {
-    const authHeader = req.headers['authorization'];
-
+    const authHeader = req.headers['Authorization'];
     if (!authHeader || authHeader !== process.env.INTELLIZON_FRONT_API_KEY) {
         return res.status(401).send('Unauthorized');
     }
@@ -45,16 +50,53 @@ app.get('/api/intellizon-front/getLatestData', authFront, async (req, res) => {
     } catch (error) {
         console.error("Une erreur est survenue lors de la récupération des dernières informations dans MongoDB :", error);
         res.status(500).send('Internal server error');
-
     } finally {
         await mongoClient.close();
     }
 });
 
+// Récupérer les données 
+app.post('/api/intellizon-front/getData', async (req, res) => {
+    try {
+        const body = req.body;
+
+        const start = body.start || null;
+        const end = body.end || null;
+
+        await mongoClient.connect();
+
+        const database = mongoClient.db('intellizon_helium');
+        const collection = database.collection('sensor_data');
+
+        // Construire l'objet de requête, si un START et/ou un END sont définis dans le corps de la requête
+        const query = {};
+        if (start || end) {
+            query.datetime = {};
+        }
+        if (start) {
+            query.datetime.$gte = new Date(start);
+        }
+        if (end) {
+            query.datetime.$lte = new Date(end);
+        }
+
+        const cursor = collection.find(query);
+        const documents = await cursor.toArray();
+        res.status(200).send(documents);
+
+    } catch (error) {
+        console.error('Error retrieving data from MongoDB:', error);
+        res.status(500).send('Internal Server Error');
+    } finally {
+        await mongoClient.close();
+    }
+});
+
+
 // Enregistrer des données à jour
 app.post('/api/helium/saveData', authHelium, async (req, res) => {
     try {
-        const timeISO = req.body.time;
+        const timeISO = new Date(req.body.time);
         const humidity = req.body.object.humidity;
         const temperature = req.body.object.temp;
 
@@ -78,17 +120,13 @@ app.post('/api/helium/saveData', authHelium, async (req, res) => {
                 unit: '°C'
             }
         };
-
         const result = await collection.insertOne(data);
-
         console.log("Données insérées avec l'id :", result.insertedId.toString());
-
         res.status(200).send('OK');
 
     } catch (error) {
         console.error("Une erreur est survenue lors de l'enregistrement dans MongoDB :", error);
         res.status(500).send('Internal Server Error');
-
     } finally {
         await mongoClient.close();
     }
