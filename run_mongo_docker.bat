9bdeefb83e13d7cc86637dd8ad7a10c1670486ea
Milan NICOLAS <milan.nicolas@hotmail.fr>
2024-04-17 22:59:24 +0200

    Fix README + Vulnerability in MongoDB batch runner
    * Fixed global port forwarding vulnerability
    
    Note : Don't forget to change MONGO_INITDB_ROOT_USERNAME and MONGO_INITDB_ROOT_PASSWORD !

--- run_mongo_docker.bat
+++ run_mongo_docker.bat
@@ -2,7 +2,7 @@
 
 docker run ^
     --name helium_mongo ^
-    -p 27017:27017 ^
+    -p 127.0.0.1:27017:27017 ^
     -e MONGO_INITDB_ROOT_USERNAME="admin" ^
     -e MONGO_INITDB_ROOT_PASSWORD="admin" ^
     -d mongo
